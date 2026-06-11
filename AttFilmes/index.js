85f9dcbd6b7a3b7b66c7c853cb7e507543b713ac
BIXPART <163024871+BIXPART@users.noreply.github.com>
2026-06-11 08:58:54 -0300

    des uso da caçamba do .then

--- AttFilmes/index.js
+++ AttFilmes/index.js
@@ -7,9 +7,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(routes);
 
-routes(app)
+routes(app);
 
-app.listen(port,()=>{console.log("Rodando na porta: ",port);
-})
+app.listen(port, () => {
+  console.log("Rodando na porta:", port);
+});
